a84796834f9f37f28197d6fbad69acaee9dfbe0f
Yuu <enstyle.enfitl@gmail.com>
2026-05-18 23:57:05 +0900

    feat: aggregate owner tasks

--- app/owner/tasks/page.tsx
+++ app/owner/tasks/page.tsx
@@ -1,3 +1,18 @@
-import TasksPage from "@/app/tasks/page";
+import { memberRepository, taskRepository } from "@/lib/repositories";
+import { OWNER_STORE_IDS, getTrialStoreNameForData } from "@/lib/trialStore";
+import TasksClient from "@/app/tasks/TasksClient";
 
-export default TasksPage;
+export default async function OwnerTasksPage() {
+  const membersByStore = await Promise.all(
+    OWNER_STORE_IDS.map((id) =>
+      memberRepository.getAllForStore(getTrialStoreNameForData(id))
+    )
+  );
+  const initialMembers = membersByStore.flat();
+
+  const ownerMemberIds = new Set(initialMembers.map((m) => m.id));
+  const allTasks = await taskRepository.getAll();
+  const initialTasks = allTasks.filter((t) => ownerMemberIds.has(t.memberId));
+
+  return <TasksClient initialTasks={initialTasks} initialMembers={initialMembers} />;
+}
