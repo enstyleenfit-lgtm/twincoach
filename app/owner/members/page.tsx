85a897fc0573c1e7fd5d6d0c2771aad24010f2b5
Yuu <enstyle.enfitl@gmail.com>
2026-05-18 23:51:07 +0900

    feat: aggregate owner members

--- app/owner/members/page.tsx
+++ app/owner/members/page.tsx
@@ -1,3 +1,14 @@
-import MembersPage from "@/app/members/page";
+import { memberRepository } from "@/lib/repositories";
+import { sortMembersByDisplayName } from "@/lib/sortMembersByName";
+import { OWNER_STORE_IDS, getTrialStoreNameForData } from "@/lib/trialStore";
+import MembersClient from "@/app/members/MembersClient";
 
-export default MembersPage;
+export default async function OwnerMembersPage() {
+  const membersByStore = await Promise.all(
+    OWNER_STORE_IDS.map((id) =>
+      memberRepository.getAllForStore(getTrialStoreNameForData(id))
+    )
+  );
+  const initialMembers = sortMembersByDisplayName(membersByStore.flat());
+  return <MembersClient initialMembers={initialMembers} />;
+}
